0357b424166d620452da2d39d8f74984e470248f
Sonia Volpe <sonia.volpe@frvr.com>
2025-10-03 10:43:20 +0200

    feat: add a new diffusion pipeline model from HF

--- api/image/models.py
+++ api/image/models.py
@@ -1,13 +1,18 @@
 import torch
 from diffusers import DiffusionPipeline, StableDiffusionInpaintPipelineLegacy
+from huggingface_hub import login
 from PIL import Image
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+login(token=os.environ.get("HF_TOKEN"))
 
 
 def load_image_model() -> StableDiffusionInpaintPipelineLegacy:
     pipe = DiffusionPipeline.from_pretrained(
-        "segmind/tiny-sd", torch_dtype=torch.float32, device=device
+        "black-forest-labs/FLUX.1-dev",
+        device=device,
     )
     return pipe
 
@@ -15,5 +20,14 @@ def load_image_model() -> StableDiffusionInpaintPipelineLegacy:
 def generate_image(
     pipe: StableDiffusionInpaintPipelineLegacy, prompt: str
 ) -> Image.Image:
-    output = pipe(prompt, num_inference_steps=10).images[0]
+    # output = pipe(prompt, num_inference_steps=10).images[0]
+    output = pipe(prompt).images[0]
     return output
+
+
+# from diffusers import DiffusionPipeline
+
+# pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev")
+
+# prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
+# image = pipe(prompt).images[0]
